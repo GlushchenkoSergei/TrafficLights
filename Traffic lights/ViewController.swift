1a33910799b97bf6a043bf652ec14f9486e36f37
Sergei <glushenko6314@icloud.com>
2022-03-25 21:40:24 +0300

    Initial Commit

---
+++ Traffic lights/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Traffic lights
+//
+//  Created by mac on 25.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
